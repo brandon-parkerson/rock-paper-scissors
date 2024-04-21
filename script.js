48c55be3af8d15bb4da7f1b87eeec3f4b7b3f80e
brandon-parkerson <166299546+brandon-parkerson@users.noreply.github.com>
2024-04-21 17:48:10 +0300

    Add buttons and keep score with JS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Have an array of answers
 const answers = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 // Get the computers choice by selcting from the array randomly and return that choice
 let computer = function computerSelection() {
@@ -16,106 +18,90 @@ let player = function playerSelection() {
 
 //  Play a round by comparing both players' choices and print the outcome and return the outcome
 function playRound(player, computer) {
-    player = player()
-    computer = computer()
+    
+    computer = computer();
+    const div = document.querySelector(".result");
+    const computerResult = document.querySelector(".computerResult")
+    const userScore = document.querySelector(".score")
+    const comScore = document.querySelector(".comScore");
+    
+
+    
+    userScore.textContent = `Your score: ${playerScore}`;
+    comScore.textContent = `Computer score ${computerScore}`;
+    
 
     if (computer == `rock` && player == `paper`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`player wins`);
-        
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `player wins`;
+        playerScore += 1;
+        if (playerScore == 5) {
+            div.textContent = `player wins the whole game`;
+        }
         return `player wins`;
     }
     else if (computer == `rock` && player == `scissors`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`computer wins`);
-        
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `computer wins`;
+        computerScore += 1;
+        if (computerScore == 5) {
+            div.textContent = `computer wins the whole game`;
+            return `computer wins`
+        }
         return 'computer wins';
     }
     else if (computer == `rock` && player == `rock`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`Draw`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `Draw`;
 
         return `draw`;
     }
     else if (computer == `paper` && player == `rock`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`computer wins`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `computer wins`;
         
         return `computer wins`;
     }
     else if (computer == `paper` && player == `paper`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`Draw`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `Draw`;
 
         return `draw`;
     }
     else if (computer == `paper` && player == `scissors`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`player wins`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `player wins`;
         
         return `player wins`;
     }
     else if (computer == `scissors` && player == `rock`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`player wins`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `player wins`;
         
         return `player wins`;
     }
     else if (computer == `scissors` && player == `paper`) {
-        console.log(`computer chose ${computer}`)
-        console.log(`computer wins`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `computer wins`;
         
         return `computer wins`;
     }
     else {
-        console.log(`computer chose ${computer}`)
-        console.log(`Draw`);
+        computerResult.textContent = `computer chose ${computer}`;
+        div.textContent = `Draw`;
 
         return `draw`;
     }
+    
 
 };
 
 // Play the game by having 5 rounds. Keep score. Add a point to the score whether user or computer wins round. 
 // Display the winner depending on who has more points.
 function playGame() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let x  = playRound(player, computer);
-    if (x == `player wins`) {
-        playerScore++;
-    }
-    else if (x == `computer wins`) {
-        computerScore++;
-    }
-    let y = playRound(player, computer);
-    if (y == `player wins`) {
-        playerScore++;
-    }
-    else if (y == `computer wins`) {
-        computerScore++;
-    }
-    z = playRound(player, computer);
-    if (z == `player wins`) {
-        playerScore++;
-    }
-    else if (z == `computer wins`) {
-        computerScore++;
-    }
-    let a = playRound(player, computer);
-    if (a == `player wins`) {
-        playerScore++;
-    }
-    else if (a == `computer wins`) {
-        computerScore++;
-    }
-    let b = playRound(player, computer);
-    if (b == `player wins`) {
-        playerScore++;
-    }
-    else if (b == `computer wins`) {
-        computerScore++;
-    }
+    const winner = document.querySelector(".winner");
+    
+    
     if (playerScore > computerScore) {
         console.log(`player wins the game`);
     }
@@ -125,6 +111,22 @@ function playGame() {
     console.log(`You're score: ${playerScore}`);
     console.log(`Computer's score: ${computerScore}`);
 };
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+rock.addEventListener("click", () => {
+    playRound(`rock`, computer);
+});
+
+paper.addEventListener("click", () => {
+    playRound(`paper`, computer);
+});
+
+scissors.addEventListener("click", () => {
+    playRound(`scissors`, computer);
+});
 
 // call the game funciton
 playGame();
